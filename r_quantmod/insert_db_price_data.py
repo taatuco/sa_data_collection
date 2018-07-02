ad7081803385546dffc29a237870a701d4257c27
Daiviet Huynh <daiviet.huynh@gmail.com>
2018-07-02 05:18:50 +0000

    update on data collection. Synchronise changes

--- r_quantmod/insert_db_price_data.py
+++ r_quantmod/insert_db_price_data.py
@@ -2,7 +2,7 @@
 # Desc: Read csv and update the database accordingly: table: price_historical_data
 #
 # Read csv file and insert records that are not existing in the database table
-# price_historical_data. (Avoid duplicate records).
+# price_instruments_data. (Avoid duplicate records).
 #
 # Dependencies: PyMySQL is required to access MySQL database.
 #
@@ -16,9 +16,10 @@ db_pwd = '9XHWVxTH9ZJnshvN'
 db_name = 'smartalpha'
 db_srv = 'localhost'
 
-# Use csv
+# Use csv and file system
 import csv
 csvdir = "C:\\xampp\\htdocs\\_sa\\sa_data_collection\\r_quantmod\\src\\"
+from pathlib import Path
 
 # Use PyMySQL to access MySQL database
 import pymysql.cursors
@@ -33,18 +34,47 @@ connection = pymysql.connect(host=db_srv,
 # Get symbol_list to iterate for records to collect
 try:
     with connection.cursor() as cursor:
-        # Read a single record
+        # Read symbol_list
         sql = "SELECT * FROM symbol_list"
         cursor.execute(sql)
         result = cursor.fetchall()
         for row in result:
-            # Read csv file
-            with open(csvdir+row["symbol"]+'csv') as csvfile:
-                readCSV = csv.reader(csvfile, delimiter=',')
-                for row in readCSV:
-                    print(row)
-                    print(row[0])
-                    print(row[0],row[1],row[2],)
-            # For each symbol, retrieve the csv content
+            symbol_quantmod = row["r_quantmod"]
+            symbol_index = row["symbol"]
+            file_str = csvdir+symbol_quantmod+'.csv'
+            filepath = Path(file_str)
+            if filepath.exists():
+                # Read csv file
+                with open(file_str) as csvfile:
+                    readCSV = csv.reader(csvfile, delimiter=',')
+                    for row in readCSV:
+                        # For each symbol, retrieve the csv content
+                        price_date = row[0]
+                    
+                        price_date = price_date.replace('.', '-')
+                        price_date = price_date.replace('X', '')
+                        price_date = price_date.replace('-','')
+                        price_date = '%.8s' % price_date
+                        price_open = row[1]
+                        price_high = row[2]
+                        price_low = row[3]
+                        price_close = row[4]
+                        volume = row[5]
+                        # check for each row if not already exists.
+                        # if exists, then insert new record, else ignore.
+                        if price_open != "open":
+                            with connection.cursor() as query_count_cursor:
+                                query_count_sql = "SELECT * FROM price_instruments_data WHERE symbol='"+symbol_index+"' AND 'date'='"+price_date+"'"
+                                query_count_cursor.execute(query_count_sql)
+                                exists_rec = query_count_cursor.fetchone()
+                        
+                            if not exists_rec:
+                                # insert record in case not existing.
+                                with connection.cursor() as query_insert_cursor:
+                                    insert_price_sql = "INSERT INTO price_instruments_data (symbol, date, price_close, price_open, price_low, price_high, volume) VALUES ('"+symbol_index+"',"+price_date+","+price_close+","+price_open+","+price_low+","+price_high+","+volume+");"
+                                    #print(insert_price_sql)
+                                    query_insert_cursor.execute(insert_price_sql)
+                                    connection.commit()
+
 finally:
     connection.close()
