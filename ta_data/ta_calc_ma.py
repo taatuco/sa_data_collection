08dd48d06df547a3a6b351779373c87256700e33
Daiviet Huynh <daiviet.huynh@gmail.com>
2018-07-06 11:07:29 +0000

    ma200 file

--- ta_data/ta_calc_ma.py
+++ ta_data/ta_calc_ma.py
@@ -16,7 +16,6 @@ db_pwd = access_obj.password()
 db_name = access_obj.db_name()
 db_srv = access_obj.db_server()
 
-
 # Use PyMySQL to access MySQL database
 import pymysql.cursors
 
@@ -27,40 +26,63 @@ connection = pymysql.connect(host=db_srv,
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-# Get symbol_list to iterate for records to collect
-try:
-    with connection.cursor() as cursor:
-        # Read symbol_list
-        sql = "SELECT * FROM symbol_list"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        for row in result:
-            symbol_index = row["symbol"]
-            # Select only rows from price_instruments_data with ma200
-            with connection.cursor() as cursor_price_data:
-                sql_price_data = "SELECT symbol, date, ma200 FROM price_instruments_data "+\
-                      "WHERE symbol='"+symbol_index+"' AND ma200=0 ORDER BY date DESC"
-                cursor_price_data.execute(sql_price_data)
-                result_price_data = cursor_price_data.fetchall()
-                for row in result_price_data:
-                    date_index = str(row["date"]).replace("-","")
-                    # For each date get the average price for 200 period
-                    with connection.cursor() as cursor_price_avg:
-                        sql_price_avg = "SELECT AVG(price_close) AS avg200 "+\
-                                        "FROM (SELECT symbol, date, ma200, price_close "+\
-                                        "FROM price_instruments_data WHERE symbol='"+symbol_index+"' AND "+\
-                                        "date<"+date_index+" LIMIT 200) avg_table"
-                        cursor_price_avg.execute(sql_price_avg)
-                        result_price_avg = cursor_price_avg.fetchall()
-                        for row in result_price_avg:
-                            ma200 = str(row["avg200"])
-                            # Update ma200 column
-                        with connection.cursor() as cursor_ma200_update:
-                            sql_ma200_update = "UPDATE price_instruments_data "+\
-                                               "SET ma200="+ma200 +\
-                                               " WHERE symbol='"+symbol_index+"' AND date="+ date_index
-                            cursor_ma200_update.execute(sql_ma200_update)
-                            connection.commit()
+def drop_tmp_v_view():
+    # Drop the view
+    try:
+        with connection.cursor() as cursor_drop_view:
+            sql_drop_view = "DROP VIEW tmp_v_ma200"
+            cursor_drop_view.execute(sql_drop_view)
+            connection.commit()
+    except:
+        pass
 
+try:
+    # Get list of symbols
+    with connection.cursor() as cursor_list_symbol:
+        sql_list_symbol = "SELECT * FROM symbol_list"
+        cursor_list_symbol.execute(sql_list_symbol)
+        result_list_symbol = cursor_list_symbol.fetchall()
+        for row in result_list_symbol:
+            symbol_index = row["symbol"]
+            # Get each date
+            with connection.cursor() as cursor_list_price:
+                sql_list_price = "SELECT symbol, date, price_close, ma200 "+\
+                                 "FROM price_instruments_data "+\
+                                 "WHERE symbol = '"+symbol_index+"' AND ma200 = 0 AND price_type='p' ORDER BY symbol, date DESC"
+                cursor_list_price.execute(sql_list_price)
+                result_list_price = cursor_list_price.fetchall()
+                for row in result_list_price:
+                    date_index = str(row["date"]).replace("-","")
+                    drop_tmp_v_view()
+                    # Create temporary SQL view to get ma200 value
+
+                    with connection.cursor() as cursor_create_view:
+                        sql_create_view = "CREATE VIEW tmp_v_ma200 AS "+\
+                              "(SELECT symbol, avg(price_close) as 'ma200' "+\
+                              "FROM price_instruments_data "+\
+                              "WHERE Symbol = '"+symbol_index+"' AND Date<"+date_index+" "+\
+                              "LIMIT 200)"
+                        cursor_create_view.execute(sql_create_view)
+                        connection.commit()
+                    # Collect value from the view
+                    with connection.cursor() as cursor_get_value:
+                        sql_get_value = "SELECT * FROM tmp_v_ma200"
+                        cursor_get_value.execute(sql_get_value)
+                        result_get_value = cursor_get_value.fetchall()
+                        for row in result_get_value:
+                            ma200_str = str(row["ma200"])
+                    try:
+                        # Update value in price_instruments_data
+                        with connection.cursor() as cursor_update:
+                            sql_update = "UPDATE price_instruments_data "+\
+                                         "SET ma200 = "+ ma200_str +" WHERE symbol ='"+symbol_index+"' AND date = "+date_index
+                            cursor_update.execute(sql_update)
+                            connection.commit()
+                    except:
+                        pass
+
+                    # Remove view
+                    drop_tmp_v_view()
+                    
 finally:
     connection.close()
