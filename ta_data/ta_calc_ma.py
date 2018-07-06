931ec53200a3572a654535a2a2e962c102e10cc9
Daiviet Huynh <dh@taatu.co>
2018-07-06 22:31:08 +0700

    TA py calc functions

--- ta_data/ta_calc_ma.py
+++ ta_data/ta_calc_ma.py
@@ -3,29 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-#import db access object
-import sys
-import os
-sys.path.append(os.path.abspath("C:\\xampp\\htdocs\\_sa\\sa_pwd"))
-from sa_access import *
-access_obj = sa_db_access()
-
-#define database username and password and other variable regarding access to db
-db_usr = access_obj.username()
-db_pwd = access_obj.password()
-db_name = access_obj.db_name()
-db_srv = access_obj.db_server()
-
-# Use PyMySQL to access MySQL database
-import pymysql.cursors
-
-connection = pymysql.connect(host=db_srv,
-                             user=db_usr,
-                             password=db_pwd,
-                             db=db_name,
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
 def drop_tmp_v_view():
     # Drop the view
     try:
@@ -37,6 +14,28 @@ def drop_tmp_v_view():
         pass
 
 def calc_ma():
+    #import db access object
+    import sys
+    import os
+    sys.path.append(os.path.abspath("C:\\xampp\\htdocs\\_sa\\sa_pwd"))
+    from sa_access import *
+    access_obj = sa_db_access()
+
+    #define database username and password and other variable regarding access to db
+    db_usr = access_obj.username()
+    db_pwd = access_obj.password()
+    db_name = access_obj.db_name()
+    db_srv = access_obj.db_server()
+
+    # Use PyMySQL to access MySQL database
+    import pymysql.cursors
+
+    connection = pymysql.connect(host=db_srv,
+                                 user=db_usr,
+                                 password=db_pwd,
+                                 db=db_name,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
     try:
         # Get list of symbols
         with connection.cursor() as cursor_list_symbol:
