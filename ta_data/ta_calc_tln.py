f90ea85a2d6396d17f75d243c2a8b64f9064c7a5
Daiviet Huynh <dh@taatu.co>
2018-07-24 07:42:19 +0700

    nightly work

--- ta_data/ta_calc_tln.py
+++ ta_data/ta_calc_tln.py
@@ -16,6 +16,13 @@ db_pwd = access_obj.password()
 db_name = access_obj.db_name()
 db_srv = access_obj.db_server()
 
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 from datetime import datetime, timedelta
 
 class trend_pts:
@@ -34,12 +41,6 @@ class trend_pts:
         self.p = p;
         self.p2 = p/2;
 
-        connection = pymysql.connect(host=db_srv,
-                                     user=db_usr,
-                                     password=db_pwd,
-                                     db=db_name,
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
         with connection.cursor() as cr:
             sql = "SELECT symbol, date FROM price_instruments_data WHERE symbol='"+ self.s +"' ORDER BY date DESC LIMIT 1"
             cr.execute(sql)
@@ -62,6 +63,17 @@ class trend_pts:
     def get_x1(self):
         self.x1 = d - timedelta(days=1)
         return self.x1
+
+    def get_val_frm_d(self,d,get_what):
+        #get from date
+        rv = 0
+        with connection.cursor() as cr:
+            sql = "SELECT date FROM price_instruments_data WHERE symbol='"+self.s+"' AND date="+d+" LIMIT 1"
+            cr.execute(sql)
+            rs = cr.fetchall()
+            for row in rs:
+                rv =
+
 
 
 class tln_data:
@@ -88,9 +100,14 @@ class tln_data:
         md = self.md
         x1 = self.x1
         p = self.p
+        get_this = self.get_this
         x = 0
 
         #get the value
+        sdv = get_val_frm_d(sd, get_this)
+        edv = get_val_frm_d(ed, get_this)
+        mdv = get_val_frm_d(md, get_this)
+        x1v = get_val_frm_d(x1, get_this)
 
         if sdv > edv:
             x = x1v + ( (edv - sdv)/p )
