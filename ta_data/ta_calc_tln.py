0838933a0561f541765ec67d815db85d92ffb70f
Botchai <info@taatu.co>
2018-07-29 12:20:50 +0700

    print error from TL output

--- ta_data/ta_calc_tln.py
+++ ta_data/ta_calc_tln.py
@@ -47,7 +47,7 @@ class trend_pts:
             rs = cr.fetchall()
             for row in rs:
                 self.ed = row["date"]
-        self.sd = self.ed - timedelta(days=self.p)                
+        self.sd = self.ed - timedelta(days=self.p)
         self.md = self.ed - timedelta(days=self.p2)
 
     def get_sd(self):
@@ -80,7 +80,7 @@ class trend_pts:
             for row in rs:
                 v = row["p"]
         return v
-    
+
 
 
 class tln_data:
@@ -92,9 +92,11 @@ class tln_data:
     sdv = 0
     edv = 0
     p = 0
+    s = ""
 
     def __init__(self, symbol_id, period, get_what):
         pts = trend_pts(symbol_id, period)
+        self.s = symbol_id
         self.sd = pts.get_sd()
         self.ed = pts.get_ed()
         self.md = pts.get_md()
@@ -108,7 +110,7 @@ class tln_data:
         try:
             if x1v == 0:
                 x1v = self.sdv
-            
+
             if d >= self.sd and d != self.ed:
                 if self.sdv > self.edv:
                     x = x1v + ( (self.edv - self.sdv)/self.p )
@@ -118,8 +120,8 @@ class tln_data:
                 x = self.edv
             else:
                 x = 0
-        except Exception as e: print(e)
-        
+        except Exception as e: print(self.s +": "+ str(d) +" = " + e)
+
         return x
 
 def get_trend_line_data(s):
@@ -130,12 +132,12 @@ def get_trend_line_data(s):
     tl_360_h = tln_data(s,360,"h")
     dpts = trend_pts(s,360)
     t180_l_x1v = 0
-    t180_h_x1v = 0    
+    t180_h_x1v = 0
     t360_l_x1v = 0
-    t360_h_x1v = 0    
+    t360_h_x1v = 0
     sd = dpts.get_sd()
     f = "src\\"+ s.replace(":","_") +"_tl.csv"
-       
+
     with connection.cursor() as cr:
         sql = "SELECT date, price_close "+\
                 "FROM price_instruments_data "+\
@@ -155,13 +157,9 @@ def get_trend_line_data(s):
                 t180_h = tl_180_h.get_pts(d,t180_h_x1v)
                 t180_l_x1v = t180_l
                 t180_h_x1v = t180_h
-                
+
                 t360_l = tl_360_l.get_pts(d,t360_l_x1v)
                 t360_h = tl_360_h.get_pts(d,t360_h_x1v)
                 t360_l_x1v = t360_l
                 t360_h_x1v = t360_h
                 writer.writerow({"date": str(d), "180_low": t180_l, "180_high": t180_h, "360_low": t360_l, "360_high": t360_h})
-
-
-
-            
