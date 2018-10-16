ec97d10cb3c760a922c2f4279e145479c7f256f4
daivieth <daiviet.huynh@gmail.com>
2018-10-16 19:13:16 +0700

    add feed type

---
+++ feed/add_feed_type.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018-present, Taatu Ltd.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import sys
+import os
+
+pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(pdir) )
+from settings import *
+sett = sa_path()
+
+sys.path.append(os.path.abspath( sett.get_path_pwd() ))
+from sa_access import *
+access_obj = sa_db_access()
+
+db_usr = access_obj.username()
+db_pwd = access_obj.password()
+db_name = access_obj.db_name()
+db_srv = access_obj.db_server()
+
+import csv
+csvdir = sett.get_path_r_quantmod_src()
+from pathlib import Path
+
+import pymysql.cursors
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+def add_feed_type(id,ft):
+    try:
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "INSERT INTO feed_type(id, feed_type) VALUES ('"+str(id)+"','"+ft+"')"
+        cr.execute(sql)
+        connection.commit()
+    except:
+        pass
