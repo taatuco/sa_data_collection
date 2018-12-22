141726504ac3b97f4b6552745c33af0bfb774aae
daivieth <daiviet.huynh@gmail.com>
2018-12-22 14:06:15 +0700

    dev

--- labels/set_recomm_text_lang.py
+++ labels/set_recomm_text_lang.py
@@ -63,10 +63,10 @@ sql = "INSERT INTO recommendations(lang, price_under_200ma, price_above_200ma, "
 "rsi_oversold, rsi_overbought, rsi_weak, rsi_strong, "+\
 "uptrend_recomm, downtrend_recomm, portf_descr, portf_recomm_buy, portf_recomm_sell) VALUES "+\
 "('"+lang_en+"', '"+price_under_200ma_en+"', '"+price_above_200ma_en+"', "+\
- "'"+st_upper_range_above_price_range_en+"', '"+st_lower_range_below_price_range_en+"', '"+upper_range_below_price_downtrend_en+"', "+\
- "'"+lower_range_above_price_uptrend_en+"', '"+rsi_oversold_en+"', '"+rsi_overbought_en+"', '"+rsi_weak_en+"', "+\
- "'"+rsi_strong_en+"', '"+uptrend_recomm_en+"', '"+downtrend_recomm_en+"', '"+portf_descr_en+"', '"+portf_recomm_buy_en+ "', '"+portf_recomm_sell_en+"')"
- print(sql)
+"'"+st_upper_range_above_price_range_en+"', '"+st_lower_range_below_price_range_en+"', '"+upper_range_below_price_downtrend_en+"', "+\
+"'"+lower_range_above_price_uptrend_en+"', '"+rsi_oversold_en+"', '"+rsi_overbought_en+"', '"+rsi_weak_en+"', "+\
+"'"+rsi_strong_en+"', '"+uptrend_recomm_en+"', '"+downtrend_recomm_en+"', '"+portf_descr_en+"', '"+portf_recomm_buy_en+ "', '"+portf_recomm_sell_en+"')"
+print(sql)
 
 try:
     cr.execute(sql)
