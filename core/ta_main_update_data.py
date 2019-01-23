d0d27aa689fc8c7c2316917cf6f61661f54b8d3e
daivieth <daiviet.huynh@gmail.com>
2019-01-23 13:46:13 +0700

    dev

--- core/ta_main_update_data.py
+++ core/ta_main_update_data.py
@@ -34,91 +34,87 @@ db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access
 
 import pymysql.cursors
 
-try:
-    connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd,db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-    cr = connection.cursor(pymysql.cursors.SSCursor)
-    sql = "SELECT symbol_list.symbol, symbol_list.uid, instruments.asset_class "+\
-    "FROM symbol_list JOIN instruments ON symbol_list.symbol = instruments.symbol  WHERE symbol_list.symbol NOT LIKE '"+get_portf_suffix()+"%' ORDER BY symbol"
-    cr.execute(sql)
-    rs = cr.fetchall()
-    for row in rs:
-        s = row[0]
-        uid = row[1]
-        asset_class = row[2]
+connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd,db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+cr = connection.cursor(pymysql.cursors.SSCursor)
+sql = "SELECT symbol_list.symbol, symbol_list.uid, instruments.asset_class "+\
+"FROM symbol_list JOIN instruments ON symbol_list.symbol = instruments.symbol  WHERE symbol_list.symbol NOT LIKE '"+get_portf_suffix()+"%' ORDER BY symbol"
+cr.execute(sql)
+rs = cr.fetchall()
+for row in rs:
+    s = row[0]
+    uid = row[1]
+    asset_class = row[2]
 
-        cr_pip = connection.cursor(pymysql.cursors.SSCursor)
-        sql_pip = "SELECT pip FROM instruments WHERE symbol ='"+ s +"' "
-        cr_pip.execute(sql_pip)
-        rs_pip = cr_pip.fetchall()
-        for row in rs_pip:
-            pip = row[0]
-        cr_pip.close()
+    cr_pip = connection.cursor(pymysql.cursors.SSCursor)
+    sql_pip = "SELECT pip FROM instruments WHERE symbol ='"+ s +"' "
+    cr_pip.execute(sql_pip)
+    rs_pip = cr_pip.fetchall()
+    for row in rs_pip:
+        pip = row[0]
+    cr_pip.close()
 
 
-        print(s +": "+ str(pip) +": "+ os.path.basename(__file__) )
-        dn = datetime.datetime.now() - timedelta(days=10)
-        dn = dn.strftime("%Y%m%d")
-        d = datetime.datetime.now() - timedelta(days=720)
-        d = d.strftime("%Y%m%d")
+    print(s +": "+ str(pip) +": "+ os.path.basename(__file__) )
+    dn = datetime.datetime.now() - timedelta(days=10)
+    dn = dn.strftime("%Y%m%d")
+    d = datetime.datetime.now() - timedelta(days=720)
+    d = d.strftime("%Y%m%d")
 
-        cr_d_id = connection.cursor(pymysql.cursors.SSCursor)
-        sql_d_id = "SELECT id, date FROM price_instruments_data "+\
-        "WHERE (symbol='"+s+"' and date>"+d+" and is_ta_calc=0) ORDER BY date ASC"
-        cr_d_id.execute(sql_d_id)
-        rs_d = cr_d_id.fetchall()
-        for row in rs_d:
-            d = str(row[1]).replace("-","")
-            id = row[0]
-            rsi = rsi_data(s,d,14)
-            change_1d = rsi.get_change()
-            gain_1d = rsi.get_gain()
-            loss_1d = rsi.get_loss()
-            avg_gain = rsi.get_avg_gain()
-            avg_loss = rsi.get_avg_loss()
-            rs14 = rsi.get_rs()
-            rsi14 = rsi.get_rsi()
-            rsi_overbought = rsi.get_rsi_overbought()
-            rsi_oversold = rsi.get_rsi_oversold()
-            ma200 = calc_ma(s,d,200)
-            is_ta_calc = "1"
+    cr_d_id = connection.cursor(pymysql.cursors.SSCursor)
+    sql_d_id = "SELECT id, date FROM price_instruments_data "+\
+    "WHERE (symbol='"+s+"' and date>"+d+" and is_ta_calc=0) ORDER BY date ASC"
+    cr_d_id.execute(sql_d_id)
+    rs_d = cr_d_id.fetchall()
+    for row in rs_d:
+        d = str(row[1]).replace("-","")
+        id = row[0]
+        rsi = rsi_data(s,d,14)
+        change_1d = rsi.get_change()
+        gain_1d = rsi.get_gain()
+        loss_1d = rsi.get_loss()
+        avg_gain = rsi.get_avg_gain()
+        avg_loss = rsi.get_avg_loss()
+        rs14 = rsi.get_rs()
+        rsi14 = rsi.get_rsi()
+        rsi_overbought = rsi.get_rsi_overbought()
+        rsi_oversold = rsi.get_rsi_oversold()
+        ma200 = calc_ma(s,d,200)
+        is_ta_calc = "1"
 
-            try:
-                cr_upd = connection.cursor(pymysql.cursors.SSCursor)
-                sql_upd = "UPDATE price_instruments_data SET "+\
-                "change_1d="+str(change_1d)+", "+\
-                "gain_1d="+str(gain_1d)+", "+\
-                "loss_1d="+str(loss_1d)+", "+\
-                "avg_gain="+str(avg_gain)+", "+\
-                "avg_loss="+str(avg_loss)+", "+\
-                "rs14="+str(rs14)+", "+\
-                "rsi14="+str(rsi14)+", "+\
-                "rsi_overbought="+str(rsi_overbought)+", "+\
-                "rsi_oversold="+str(rsi_oversold)+", "+\
-                "ma200="+str(ma200)+ ", "+\
-                "is_ta_calc="+str(is_ta_calc)+" "+\
-                "WHERE id="+str(id)
-                cr_upd.execute(sql_upd)
-                connection.commit()
-                cr_upd.close()
-                print(sql_upd)
-            except:
-                sql_upd = "UPDATE price_instruments_data SET "+\
-                "is_ta_calc=1 "+\
-                "WHERE id="+str(id)
-                cr_upd.execute(sql_upd)
-                connection.commit()
-                cr_upd.close()
-        cr_d_id.close()
-        # Calc other data as per symbol
-        get_trend_line_data(s,uid)
-        get_day_up_dwn_stat(s,uid)
-        set_signals_feed(s)
-        gen_recomm(s,uid)
-        get_forecast_pnl(s,uid)
-        get_trades(s,uid,370)
-        gen_chart(s,uid)
-        get_instr_sum(s,uid,asset_class,dn,pip)
-    cr.close()
-
-finally:
-    connection.close()
+        try:
+            cr_upd = connection.cursor(pymysql.cursors.SSCursor)
+            sql_upd = "UPDATE price_instruments_data SET "+\
+            "change_1d="+str(change_1d)+", "+\
+            "gain_1d="+str(gain_1d)+", "+\
+            "loss_1d="+str(loss_1d)+", "+\
+            "avg_gain="+str(avg_gain)+", "+\
+            "avg_loss="+str(avg_loss)+", "+\
+            "rs14="+str(rs14)+", "+\
+            "rsi14="+str(rsi14)+", "+\
+            "rsi_overbought="+str(rsi_overbought)+", "+\
+            "rsi_oversold="+str(rsi_oversold)+", "+\
+            "ma200="+str(ma200)+ ", "+\
+            "is_ta_calc="+str(is_ta_calc)+" "+\
+            "WHERE id="+str(id)
+            cr_upd.execute(sql_upd)
+            connection.commit()
+            cr_upd.close()
+            print(sql_upd)
+        except:
+            sql_upd = "UPDATE price_instruments_data SET "+\
+            "is_ta_calc=1 "+\
+            "WHERE id="+str(id)
+            cr_upd.execute(sql_upd)
+            connection.commit()
+            cr_upd.close()
+    cr_d_id.close()
+    # Calc other data as per symbol
+    get_trend_line_data(s,uid)
+    get_day_up_dwn_stat(s,uid)
+    set_signals_feed(s)
+    gen_recomm(s,uid)
+    get_forecast_pnl(s,uid)
+    get_trades(s,uid,370)
+    gen_chart(s,uid)
+    get_instr_sum(s,uid,asset_class,dn,pip)
+cr.close()
