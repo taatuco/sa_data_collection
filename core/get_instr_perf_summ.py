341dc182e1b0c3c40b3ff71e6d3749ed6b4b8076
daivieth <daiviet.huynh@gmail.com>
2019-01-24 14:11:31 +0700

    dev

--- core/get_instr_perf_summ.py
+++ core/get_instr_perf_summ.py
@@ -68,7 +68,6 @@ class instr_sum_data:
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: symbol_is_portf = row[0]
-        cr.close()
         if symbol_is_portf.find( get_portf_suffix() ) > -1 :
             self.sql_select = "SELECT price_close, date FROM chart_data WHERE symbol='"+ self.s +"' "
         else:
