0485ac6e5f4cf1deff33367f54c577a9ba8155fa
daivieth <daiviet.huynh@gmail.com>
2019-02-14 13:47:30 +0700

    dev

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+import sys
 
 
 sys.path.append(os.path.abspath( sett.get_path_pwd() ))
