4149345166921fadc88d7c8897c3e446790ca963
daivieth <daiviet.huynh@gmail.com>
2019-02-14 14:09:49 +0700

    dev

--- portfolios/set_portf.py
+++ portfolios/set_portf.py
@@ -32,6 +32,21 @@ connection = pymysql.connect(host=db_srv,
                              cursorclass=pymysql.cursors.DictCursor)
 
 
+def get_user_smartalpha_id():
+
+    sa_bot_nickname = 'smartalpha'
+    r = ''
+    try:
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT id FROM users WHERE nickname = '"+ sa_bot_nickname +"'"
+        cr.execute(sql)
+        rs = cr.fetchall()
+        for row in rs: r = row[0]
+        cr.close()
+    except Exception as e: print(e)
+    return r
+
+
 portf_symbol_suffix = get_portf_suffix()
 cr = connection.cursor(pymysql.cursors.SSCursor)
 sql = "DELETE FROM instruments WHERE symbol LIKE '" + portf_symbol_suffix + "%' "
@@ -49,7 +64,6 @@ connection.commit()
 cr.close()
 
 owner_sa_bot_id = get_user_smartalpha_id()
-
 
 '''
 ### Template description for portfolio:
@@ -81,7 +95,7 @@ def set_portf_fx():
         connection.commit()
     except Exception as e: print(e)
     sql = "INSERT INTO instruments(symbol, fullname, asset_class, market, decimal_places, pip, sector, unit, description, account_reference, owner) VALUES "+\
-    "('" + portf_symbol_suffix + "FXONE','No-Fly Global Forex long/short','FX:','GO>',5,1,1,'USD','With an exposure in the foreign exchange market, the defined porfolio projects an absolute positive return in the next couple days.',1000)"
+    "('" + portf_symbol_suffix + "FXONE','No-Fly Global Forex long/short','FX:','GO>',5,1,1,'USD','With an exposure in the foreign exchange market, the defined porfolio projects an absolute positive return in the next couple days.',1000,"+ str(owner_sa_bot_id) +")"
     try:
         cr.execute(sql)
         connection.commit()
@@ -103,7 +117,7 @@ def set_portf_crypto():
         connection.commit()
     except Exception as e: print(e)
     sql = "INSERT INTO instruments(symbol, fullname, asset_class, market, decimal_places, pip, sector, unit, description, account_reference, owner) VALUES "+\
-    "('" + portf_symbol_suffix + "CRYPTONE','The Hot Potato Global Crypto long/short','CR:','GO>',5,1,2,'USD','This is a quantamental portfolio trading on some of the most lucrative financial assets, the portfolio consists of a long/short strategy on most popular cryptocurrencies.',1000)"
+    "('" + portf_symbol_suffix + "CRYPTONE','The Hot Potato Global Crypto long/short','CR:','GO>',5,1,2,'USD','This is a quantamental portfolio trading on some of the most lucrative financial assets, the portfolio consists of a long/short strategy on most popular cryptocurrencies.',1000,"+ str(owner_sa_bot_id) +")"
     try:
         cr.execute(sql)
         connection.commit()
@@ -125,7 +139,7 @@ def set_portf_commo():
         connection.commit()
     except Exception as e: print(e)
     sql = "INSERT INTO instruments(symbol, fullname, asset_class, market, decimal_places, pip, sector, unit, description, account_reference, owner) VALUES "+\
-    "('" + portf_symbol_suffix + "COMMONE','Gold Digger Global Commodities long/short','CO:','GO>',2,1,18,'USD','This portfolio includes two of the most traded commodities products.',1000)"
+    "('" + portf_symbol_suffix + "COMMONE','Gold Digger Global Commodities long/short','CO:','GO>',2,1,18,'USD','This portfolio includes two of the most traded commodities products.',1000,"+ str(owner_sa_bot_id) +")"
     try:
         cr.execute(sql)
         connection.commit()
@@ -148,8 +162,8 @@ def set_portf_multi():
         connection.commit()
     except Exception as e: print(e)
     sql = "INSERT INTO instruments(symbol, fullname, asset_class, market, decimal_places, pip, sector, unit, description, account_reference, owner) VALUES "+\
-    "('" + portf_symbol_suffix + "MULTIONE','Milkshake Global Multi-asset long/short','MA:','GO>',5,1,19,'USD','This is a quantamental portfolio trading on some of the most lucrative financial assets, a combination of relevant stocks and cryptocurrencies.',1000), "+\
-    "('" + portf_symbol_suffix + "GOJONE','Safe Haven Global Multi-asset long/short','MA:','GO>',5,1,19,'USD','This is a playsafe portfolio, involving only Gold ETF and Japanese Yen.',1000)"
+    "('" + portf_symbol_suffix + "MULTIONE','Milkshake Global Multi-asset long/short','MA:','GO>',5,1,19,'USD','This is a quantamental portfolio trading on some of the most lucrative financial assets, a combination of relevant stocks and cryptocurrencies.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "GOJONE','Safe Haven Global Multi-asset long/short','MA:','GO>',5,1,19,'USD','This is a playsafe portfolio, involving only Gold ETF and Japanese Yen.',1000,"+ str(owner_sa_bot_id) +")"
     try:
         cr.execute(sql)
         connection.commit()
@@ -187,22 +201,22 @@ def set_portf_us():
         connection.commit()
     except Exception as e: print(e)
     sql = "INSERT INTO instruments(symbol, fullname, asset_class, market, decimal_places, pip, sector, unit, description, account_reference, owner) VALUES "+\
-    "('" + portf_symbol_suffix + "INDXONE','The Escalator Global Equity long/short','EQ:','GO>',2,1,17,'USD','This portfolio includes carefully selected world major indices traded with a long/short strategy.',1000), "+\
-    "('" + portf_symbol_suffix + "INDUONEUS','Smoke Up U.S. Equity long/short','EQ:','US>',2,1,4,'USD','This portfolio is a combination of assets from different industries statistically selected to provide positive gains over the next week.',1000), "+\
-    "('" + portf_symbol_suffix + "TECHONEUS','Time Travel U.S. Equity long/short','EQ:','US>',2,1,5,'USD','A simple yet highly vetted long/short portfolio of three US equities in the Tech industry.',1000), "+\
-    "('" + portf_symbol_suffix + "HCONEUS','Nip Tuck U.S. Equity long/short','EQ:','US>',2,1,6,'USD','This portfolio of three assets scanned through the healthcare industry, with a focus to achieve the most preferable gains in less than 7 days.',1000), "+\
-    "('" + portf_symbol_suffix + "CDONEUS','Rich Kids U.S. Equity long/short','EQ:','US>',2,1,7,'USD','With an exposure in three major US equities in the consumer discretionary sector, the defined porfolio projects an absolute positive return in the next couple days.',1000), "+\
-    "('" + portf_symbol_suffix + "UTILONEUS','Uncle James U.S. Equity long/short','EQ:','US>',2,1,8,'USD','This portfolio trades on US utilities sector, having a combination of 3 financial instruments carefully selected.',1000), "+\
-    "('" + portf_symbol_suffix + "FINONEUS','Piggy Bank U.S. Equity long/short','EQ:','US>',2,1,9,'USD','Selected from a range of dynamic metrics, the selected assests are the most desirable in the finance and banking industry.',1000), "+\
-    "('" + portf_symbol_suffix + "MATONEUS','Iron String U.S. Equity long/short','EQ:','US>',2,1,10,'USD','This portfolio includes assets in the US materials sector.',1000), "+\
-    "('" + portf_symbol_suffix + "TONEUS','Snail U.S. Bonds long/short','BD:','US>',2,1,11,'USD','With an exposure in the US Treasury bonds and Gold ETF, the defined porfolio projects an absolute positive return in the next couple days.',1000), "+\
-    "('" + portf_symbol_suffix + "CSONEUS','Bread and Milk U.S. Equity long/short','EQ:','US>',2,1,12,'USD','A simple yet highly vetted long/short portfolio of three financial instruments in the consumer staples sector.',1000), "+\
-    "('" + portf_symbol_suffix + "NRGONEUS','The Fast and Furious U.S. Equity long/short','EQ:','US>',2,1,13,'USD','Selected from a range of dynamic metrics, the selected three assests are the most desirable in the energy sector to provide positive gains from a long/short strategy in the next couple of days.',1000), "+\
-    "('" + portf_symbol_suffix + "TELCONEUS','The Phone Booth U.S. Equity long/short','EQ:','US>',2,1,14,'USD','With an exposure in the telecommunications sector industry with 3 selected US equities, the defined porfolio projects an absolute positive return in the next couple days.',1000), "+\
-    "('" + portf_symbol_suffix + "REITONEUS','House of Cards U.S. Equity long/short','EQ:','US>',2,1,15,'USD','With an exposure in the real estate development industry, the defined porfolio projects an absolute positive return in the next couple days.',1000), "+\
-    "('" + portf_symbol_suffix + "FOODONEUS','Burritos U.S. Equity long/short','EQ:','US>',2,1,12,'USD','This portfolio trades on food and beverage sector, having a combination of 3 financial instruments carefully selected.',1000), "+\
-    "('" + portf_symbol_suffix + "DEFONEUS','Guns and Roses U.S. Equity long/short','EQ:','US>',2,1,4,'USD','This portfolio includes carefully selected three US stocks in the defense sector traded with a long/short strategy.',1000), "+\
-    "('" + portf_symbol_suffix + "TOBACONEUS','Party don-t stop U.S. Equity long/short','EQ:','US>',2,1,12,'USD','This portfolio with just 3 US equities in the tobacco and beverage sector not just only beat the consumer staples sector, but beat the sh*t out of you.',1000)"
+    "('" + portf_symbol_suffix + "INDXONE','The Escalator Global Equity long/short','EQ:','GO>',2,1,17,'USD','This portfolio includes carefully selected world major indices traded with a long/short strategy.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "INDUONEUS','Smoke Up U.S. Equity long/short','EQ:','US>',2,1,4,'USD','This portfolio is a combination of assets from different industries statistically selected to provide positive gains over the next week.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "TECHONEUS','Time Travel U.S. Equity long/short','EQ:','US>',2,1,5,'USD','A simple yet highly vetted long/short portfolio of three US equities in the Tech industry.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "HCONEUS','Nip Tuck U.S. Equity long/short','EQ:','US>',2,1,6,'USD','This portfolio of three assets scanned through the healthcare industry, with a focus to achieve the most preferable gains in less than 7 days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "CDONEUS','Rich Kids U.S. Equity long/short','EQ:','US>',2,1,7,'USD','With an exposure in three major US equities in the consumer discretionary sector, the defined porfolio projects an absolute positive return in the next couple days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "UTILONEUS','Uncle James U.S. Equity long/short','EQ:','US>',2,1,8,'USD','This portfolio trades on US utilities sector, having a combination of 3 financial instruments carefully selected.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "FINONEUS','Piggy Bank U.S. Equity long/short','EQ:','US>',2,1,9,'USD','Selected from a range of dynamic metrics, the selected assests are the most desirable in the finance and banking industry.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "MATONEUS','Iron String U.S. Equity long/short','EQ:','US>',2,1,10,'USD','This portfolio includes assets in the US materials sector.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "TONEUS','Snail U.S. Bonds long/short','BD:','US>',2,1,11,'USD','With an exposure in the US Treasury bonds and Gold ETF, the defined porfolio projects an absolute positive return in the next couple days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "CSONEUS','Bread and Milk U.S. Equity long/short','EQ:','US>',2,1,12,'USD','A simple yet highly vetted long/short portfolio of three financial instruments in the consumer staples sector.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "NRGONEUS','The Fast and Furious U.S. Equity long/short','EQ:','US>',2,1,13,'USD','Selected from a range of dynamic metrics, the selected three assests are the most desirable in the energy sector to provide positive gains from a long/short strategy in the next couple of days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "TELCONEUS','The Phone Booth U.S. Equity long/short','EQ:','US>',2,1,14,'USD','With an exposure in the telecommunications sector industry with 3 selected US equities, the defined porfolio projects an absolute positive return in the next couple days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "REITONEUS','House of Cards U.S. Equity long/short','EQ:','US>',2,1,15,'USD','With an exposure in the real estate development industry, the defined porfolio projects an absolute positive return in the next couple days.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "FOODONEUS','Burritos U.S. Equity long/short','EQ:','US>',2,1,12,'USD','This portfolio trades on food and beverage sector, having a combination of 3 financial instruments carefully selected.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "DEFONEUS','Guns and Roses U.S. Equity long/short','EQ:','US>',2,1,4,'USD','This portfolio includes carefully selected three US stocks in the defense sector traded with a long/short strategy.',1000,"+ str(owner_sa_bot_id) +"), "+\
+    "('" + portf_symbol_suffix + "TOBACONEUS','Party don-t stop U.S. Equity long/short','EQ:','US>',2,1,12,'USD','This portfolio with just 3 US equities in the tobacco and beverage sector not just only beat the consumer staples sector, but beat the sh*t out of you.',1000,"+ str(owner_sa_bot_id) +")"
     try:
         cr.execute(sql)
         connection.commit()
