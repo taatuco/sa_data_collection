c9d69ab5421e2069eeb584ed81a464af42b1e594
daivieth <daiviet.huynh@gmail.com>
2019-02-26 10:32:40 +0700

    dev

--- core/get_trades.py
+++ core/get_trades.py
@@ -79,9 +79,9 @@ def get_trades(s,uid,dc):
             price_close_1 = round( row[2], trade_decimal_places)
             target_price_1 = round( row[3], trade_decimal_places)
 
-            dto = date_1 + timedelta(days=7) ; dto_str = dto.strftime('%Y%m%d')
+            dto = date_1 + timedelta(days=6) ; dto_str = dto.strftime('%Y%m%d')
             cr_2 = connection.cursor(pymysql.cursors.SSCursor)
-            sql_2 = "SELECT date, price_close FROM price_instruments_data WHERE symbol = '"+ s +"' AND date >" + dto_str + " ORDER BY date LIMIT 1"
+            sql_2 = "SELECT date, price_close FROM price_instruments_data WHERE symbol = '"+ s +"' AND date >=" + dto_str + " ORDER BY date LIMIT 1"
             print(sql_2)
             cr_2.execute(sql_2)
             rs_2 = cr_2.fetchall()
