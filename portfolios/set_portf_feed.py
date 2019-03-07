c9249d81992691bbb7159aefa2cbdebb50ff2c3f
daivieth <daiviet.huynh@gmail.com>
2019-03-07 11:25:44 +0700

    dev

--- portfolios/set_portf_feed.py
+++ portfolios/set_portf_feed.py
@@ -55,15 +55,21 @@ def get_portf_ranking(s,rank):
         divider = 1
         unit = ''
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT COUNT(*), unit FROM instruments WHERE symbol ='"+ s +"' AND y1>0 "
+        sql = "SELECT unit FROM instruments WHERE symbol ='"+ s +"' "
         cr.execute(sql)
         rs = cr.fetchall()
-        for row in rs:
-            count_positive_year = row[0]
-            unit = row[1]
+        for row in rs: unit = row[0]
 
         if unit == 'pips':
             divider = pip_divider
+
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT COUNT(*) FROM instruments WHERE symbol ='"+ s +"' AND y1>0 "
+        cr.execute(sql)
+        rs = cr.fetchall()
+        for row in rs: count_positive_year = row[0]
+
+
 
         sql = "SELECT COUNT(*) FROM price_instruments_data WHERE symbol ='"+ s +"' AND price_close <= 0 "
         cr.execute(sql)
