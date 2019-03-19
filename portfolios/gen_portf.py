27140e47c45bd08e0f6bf5945c228af61b62a996
daivieth <daiviet.huynh@gmail.com>
2019-03-19 14:15:14 +0700

    dev

--- portfolios/gen_portf.py
+++ portfolios/gen_portf.py
@@ -221,7 +221,7 @@ def gen_portf():
         min_portf_threshold = 200
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT asset_class_id WHERE asset_class_id <> 'PF:' AND asset_class_id <> 'MA:' AND asset_class_id <>'BD:' AND asset_class_id <>'CO:' "
+        sql = "SELECT asset_class_id FROM asset_class WHERE asset_class_id <> 'PF:' AND asset_class_id <> 'MA:' AND asset_class_id <>'BD:' AND asset_class_id <>'CO:' "
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
