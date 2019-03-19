098d6558997db6735acc151df36e014ebf073848
daivieth <daiviet.huynh@gmail.com>
2019-03-19 14:38:25 +0700

    dev

--- portfolios/gen_portf.py
+++ portfolios/gen_portf.py
@@ -54,7 +54,7 @@ def get_portf_description(ac,m,st):
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT portf_description FROM labels WHERE lang = '"+ get_lang() +"'"
+        sql = "SELECT portf_description FROM labels WHERE lang = '"+ "en" +"'"
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: portf_description = row[0]
@@ -97,7 +97,7 @@ def get_market_name(m):
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT market_label FROM market WHERE market_id='"+ str(ac) +"' "
+        sql = "SELECT market_label FROM markets WHERE market_id='"+ str(ac) +"' "
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: r = row[0]
@@ -121,7 +121,7 @@ def get_unit(m):
     try:
         connection = pymysql.connect(host=db_srv,user=db_usr,password=db_pwd, db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "SELECT currency_code FROM market WHERE market_id = '"+ str(m) +"' "
+        sql = "SELECT currency_code FROM markets WHERE market_id = '"+ str(m) +"' "
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs: r = row[0]
