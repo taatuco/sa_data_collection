3e00d671a5bf5b3f204eb4cc3b8bfd303bb10ef9
daivieth <daiviet.huynh@gmail.com>
2019-03-24 15:03:18 +0700

    dev

--- portfolios/set_portf_feed.py
+++ portfolios/set_portf_feed.py
@@ -71,7 +71,7 @@ def get_portf_ranking(s,rank,stdev_st,y1,m6,m3,m1):
         for row in rs: account_start = row[0]
 
         drawdown_pct_threshold = 0.3
-        drawdown_account_max = account_start - (account_start * drawdown_pct_threshold)
+        drawdown_account_max = float(account_start) - (float(account_start) * float(drawdown_pct_threshold) )
         sql = "SELECT price_close FROM chart_data WHERE symbol ='"+ s +"' AND price_close < "+ drawdown_account_max + " LIMIT 1"
         cr.execute(sql)
         rs = cr.fetchall()
