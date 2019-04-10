d2fdf386430233f4b1f3ec5701e70a92215ce9ec
daivieth <daiviet.huynh@gmail.com>
2019-04-10 15:15:19 +0700

    dev

--- core/ta_main_update_data.py
+++ core/ta_main_update_data.py
@@ -61,7 +61,7 @@ def get_update_instr_data(fm,is_update_all,specific_symbol):
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
-            if is_update_all: time.sleep(1)
+            if is_update_all: time.sleep(5)
             s = row[0]
             uid = row[1]
             asset_class = row[2]
