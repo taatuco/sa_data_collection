36f3d521b72efc79585aa793485e58f0b230526a
daivieth <daiviet.huynh@gmail.com>
2019-04-10 13:34:13 +0700

    dev

--- core/ta_calc_ma.py
+++ core/ta_calc_ma.py
@@ -21,27 +21,28 @@ access_obj = sa_db_access()
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
 def calc_ma(symbol_id, date_id, ma_period):
+    try:
+        import pymysql.cursors
+        connection = pymysql.connect(host=db_srv,
+                                     user=db_usr,
+                                     password=db_pwd,
+                                     db=db_name,
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
 
-    import pymysql.cursors
-    connection = pymysql.connect(host=db_srv,
-                                 user=db_usr,
-                                 password=db_pwd,
-                                 db=db_name,
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    from_date =  datetime.datetime.strptime(date_id, '%Y%m%d') - ( timedelta(days=ma_period) )
-    from_date = from_date.strftime("%Y%m%d")
-    ma_period = str(ma_period)
-    ma = 0
-    cr = connection.cursor(pymysql.cursors.SSCursor)
-    sql = "SELECT AVG(price_close) as ma FROM price_instruments_data "+\
-    "WHERE symbol='"+symbol_id+"' AND date<="+date_id+" AND date>="+ from_date
-    cr.execute(sql)
-    rs = cr.fetchall()
-    if rs:
-        for row in rs:
-            ma = row[0]
-    cr.close()
-    connection.close()
+        from_date =  datetime.datetime.strptime(date_id, '%Y%m%d') - ( timedelta(days=ma_period) )
+        from_date = from_date.strftime("%Y%m%d")
+        ma_period = str(ma_period)
+        ma = 0
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = "SELECT AVG(price_close) as ma FROM price_instruments_data "+\
+        "WHERE symbol='"+symbol_id+"' AND date<="+date_id+" AND date>="+ from_date
+        cr.execute(sql)
+        rs = cr.fetchall()
+        if rs:
+            for row in rs:
+                ma = row[0]
+        cr.close()
+        connection.close()
+    except Exception as e: print(e)
     return(ma)
