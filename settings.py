775b0b37e7d9f6bfed7f4134055fcf6acff06253
The Cat Tamer <dh@taatu.co>
2019-08-16 09:47:18 +0700

    dev

--- settings.py
+++ settings.py
@@ -42,7 +42,7 @@ def get_product_name():
 def get_reply_to_email(w):
     r = ''
     if w == 'email':
-        r = 'no-reply@smartalphatrade.com'
+        r = 'info@taatu.co'
     if w == 'name':
         r = 'SmartAlpha Intelligence'
     return r
