0e11a0a0a1389eee7e5447c10e6af32dff131455
The Cat Tamer <dh@taatu.co>
2019-09-10 21:01:04 +0700

    dev

--- newsdata/set_newsdata.py
+++ newsdata/set_newsdata.py
@@ -49,22 +49,22 @@ sql = "INSERT IGNORE INTO newsdata(name, url, format, type, asset_class, market,
 "('Forex Live','https://www.forexlive.com/feed/news','rss','global','FX:','','en'), "+\
 "('DailyFX News','https://rss.dailyfx.com/feeds/forex_market_news','rss','global','FX:','','en'), "+\
 "('DailyFX Daily Briefing','https://rss.dailyfx.com/feeds/daily_briefings','rss','global','FX:','','en'), "+\
-"('DailyFX Forecast','https://rss.dailyfx.com/feeds/forecasts','rss','global','','','en'), "+\
-"('Realtime Forex News','https://www.realtimeforex.com/rss/','rss','global','','','en'), "+\
-"('Cointelegraph All News','https://cointelegraph.com/rss','rss','global','','','en'), "+\
-"('Cointelegraph Altcoin News','https://cointelegraph.com/rss/tag/altcoin','rss','global','','','en'), "+\
-"('Cointelegraph Bitcoin News','https://cointelegraph.com/rss/tag/bitcoin','rss','global','','','en'), "+\
-"('Cointelegraph Ethereum News','https://cointelegraph.com/rss/tag/ethereum','rss','global','','','en'), "+\
-"('Cointelegraph Litecoin News','https://cointelegraph.com/rss/tag/litecoin','rss','global','','','en'), "+\
-"('Cointelegraph Monero News','https://cointelegraph.com/rss/tag/monero','rss','global','','','en'), "+\
-"('Coindesk News','http://feeds.feedburner.com/CoinDesk','rss','global','','','en'), "+\
-"('Bitcoin com News','https://news.bitcoin.com/feed/','rss','global','','','en'), "+\
-"('Minegate News','https://minergate.com/blog/feed/','rss','global','','','en'), "+\
-"('NewsBTC','https://www.newsbtc.com/feed/','rss','global','','','en'), "+\
-"('CryptoNinja','https://www.cryptoninjas.net/feed/','rss','global','','','en'), "+\
-"('WalletInvestor','https://walletinvestor.com/blog/feed/','rss','global','','','en'), "+\
-"('Finance Magnates','https://www.financemagnates.com/cryptocurrency/feed/','rss','global','','','en'), "+\
-"('Bitcoin Magazine','https://bitcoinmagazine.com/feed','rss','global','','','en')"
+"('DailyFX Forecast','https://rss.dailyfx.com/feeds/forecasts','rss','global','FX:','','en'), "+\
+"('Realtime Forex News','https://www.realtimeforex.com/rss/','rss','global','FX:','','en'), "+\
+"('Cointelegraph All News','https://cointelegraph.com/rss','rss','global','CR:','','en'), "+\
+"('Cointelegraph Altcoin News','https://cointelegraph.com/rss/tag/altcoin','rss','global','CR:','','en'), "+\
+"('Cointelegraph Bitcoin News','https://cointelegraph.com/rss/tag/bitcoin','rss','global','CR:','','en'), "+\
+"('Cointelegraph Ethereum News','https://cointelegraph.com/rss/tag/ethereum','rss','global','CR:','','en'), "+\
+"('Cointelegraph Litecoin News','https://cointelegraph.com/rss/tag/litecoin','rss','global','CR:','','en'), "+\
+"('Cointelegraph Monero News','https://cointelegraph.com/rss/tag/monero','rss','global','CR:','','en'), "+\
+"('Coindesk News','http://feeds.feedburner.com/CoinDesk','rss','global','CR:','','en'), "+\
+"('Bitcoin com News','https://news.bitcoin.com/feed/','rss','global','CR:','','en'), "+\
+"('Minegate News','https://minergate.com/blog/feed/','rss','global','CR:','','en'), "+\
+"('NewsBTC','https://www.newsbtc.com/feed/','rss','global','CR:','','en'), "+\
+"('CryptoNinja','https://www.cryptoninjas.net/feed/','rss','global','CR:','','en'), "+\
+"('WalletInvestor','https://walletinvestor.com/blog/feed/','rss','global','CR:','','en'), "+\
+"('Finance Magnates','https://www.financemagnates.com/cryptocurrency/feed/','rss','global','CR:','','en'), "+\
+"('Bitcoin Magazine','https://bitcoinmagazine.com/feed','rss','global','CR:','','en')"
 print(sql +": "+ os.path.basename(__file__) )
 
 try:
