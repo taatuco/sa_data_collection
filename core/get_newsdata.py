7fc44874a0bf12a3a5b98a78fd42d773ebeeebf2
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-11 17:30:33 +0700

    dev

--- core/get_newsdata.py
+++ core/get_newsdata.py
@@ -14,9 +14,13 @@ sys.path.append(os.path.abspath(pdir) )
 from settings import *
 sett = sa_path()
 
+sys.path.append(os.path.abspath( sett.get_path_feed() ))
+from add_feed_type import *
+
 sys.path.append(os.path.abspath( sett.get_path_pwd() ))
 from sa_access import *
 access_obj = sa_db_access()
+
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
 
@@ -35,13 +39,47 @@ connection = pymysql.connect(host=db_srv,
 
 def get_newsdata():
     try:
-        url = 'https://www.realtimeforex.com/rss/'
-        feed = feedparser.parse(url)
 
-        for post in feed.entries:
-            print(post.title)
-            print(post.description)
-            print(post.published)
+        #Date [Today date]
+        d = datetime.datetime.now()
+        d = d.strftime("%Y%m%d")
 
+        feed_id = 3
+        feed_type = "news"
+        add_feed_type(feed_id, feed_type)
+
+    except Exception as e: print(e)
+
+def get_newsdata_global_rss(d,feed_id):
+    try:
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = 'SELECT url,format,type,asset_class,market,lang FROM newsdata WHERE type="global" AND format="rss"'
+        cr.execute(sql)
+        rs = cr.fetchall()
+
+        sep = ''
+        insert_line = ''
+        date_d = ''
+        short_title = ''
+        short_description = ''
+        url = ''
+        format = ''
+        type = str(feed_id)
+        search = ''
+        asset_class = ''
+        market = ''
+        lang = ''
+        #INSERT INTO feed(date, short_title, short_description, url, type, search, asset_class, market, lang)
+        #VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])
+        for row in rs:
+            url = row[0]
+            feed = feedparser.parse(url)
+
+            for post in feed.entries:
+                print(post.title)
+                print(post.description)
+                print(post.published)
+
+        cr.close()
 
     except Exception as e: print(e)
