4e7ca0e5daaa0d21dd65b647df32d2f0a655d00a
The Cat Tamer <dh@taatu.co>
2019-09-14 17:17:01 +0700

    dev

--- core/get_sentiment_score.py
+++ core/get_sentiment_score.py
@@ -3,14 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import json
 
 def analyze_sentiment_of_this(text):
     r = 0
     try:
         analyser = SentimentIntensityAnalyzer()
         score = analyser.polarity_scores(text)
-        data = json.loads(score)
-        r = (data['compound'])
+        r = (score.get('compound'))
         print(str(r))
     except Exception as e: print(e)
+    return r
