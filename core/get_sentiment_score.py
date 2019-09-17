f696cfa809faa80e8c264ef75eca8449b21d51ff
The Cat Tamer <dh@taatu.co>
2019-09-17 07:23:19 +0700

    dev

--- core/get_sentiment_score.py
+++ core/get_sentiment_score.py
@@ -37,18 +37,13 @@ def analyze_sentiment_of_this(text):
 def get_sentiment_score_avg(s,dh):
     r = 0
     try:
-        last_row_id = 0
         avg_sentiment = 0
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = 'SELECT id FROM price_instruments_data WHERE symbol="'+ str(s) +'" ORDER BY id DESC LIMIT 1'
-        cr.execute(sql)
-        rs = cr.fetchall()
-        for row in rs: last_row_id = row[0]
-
         sql = 'SELECT AVG(ranking) FROM feed WHERE symbol="'+ str(s) +'" AND type=3 AND date>='+ str(dh)
         cr.execute(sql)
         rs = cr.fetchall()
-        for row in rs: avg_sentiment = row[0]
+        for row in rs:
+            avg_sentiment = row[0]
+            print(avg_sentiment)
         cr.close()
 
     except Exception as e: print(e)
