8d81b3d076830fd996dd428e598afd975333e4c3
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-23 09:43:06 +0700

    dev

--- core/collect_news_data_spec_set.py
+++ core/collect_news_data_spec_set.py
@@ -2,10 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
-from ta_main_update_data import *
-from send_intel_report_email import *
 from get_newsdata import *
-get_update_instr_data(1,False,'')
-send_intel_report()
-get_newsdata(3,True,'all')
+
+get_newsdata(1,False,'specific')
