08670cfc779f20f5ee1fb447cb284e9fd85f3975
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-09-30 17:57:33 +0700

    dev

--- core/send_mail.py
+++ core/send_mail.py
@@ -95,7 +95,7 @@ def process_mail_queue():
             if from_email == '' or from_email is None:
                 from_email = get_reply_to_email('email')
                 from_email_displayname = get_reply_to_email('name')
-            if len(send_to_email_bcc) < 1:
+            if send_to_email_bcc == []:
                 send_to_email_bcc.append( get_reply_to_email('tech') )
 
             print( send_mail(from_email,from_email_displayname,send_to_email_bcc,email_subject,email_content) )
