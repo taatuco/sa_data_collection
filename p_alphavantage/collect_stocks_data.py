8cba6103d6ab588a694bff6278c9b468059ddb58
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-03 14:31:32 +0700

    dev

--- p_alphavantage/collect_stocks_data.py
+++ p_alphavantage/collect_stocks_data.py
@@ -9,7 +9,9 @@ import json
 import sys
 import os
 import gc
-from datetime import datetime
+import datetime
+import time
+from datetime import timedelta
 
 pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.abspath(pdir) )
@@ -37,9 +39,28 @@ i = 1
 api_key = 'XWOJ8KDFY4TLNYF0'
 url = "https://www.alphavantage.co/query"
 ############################################################################
-dt = datetime.datetime.now() - timedelta(days=1)
-dtsql = dt.strftime('%Y%m%d')
-dtjson = dt.strftime('%Y-%m-%d')
+dta = datetime.datetime.now() - timedelta(days=1)
+dtb = datetime.datetime.now() - timedelta(days=2)
+dtc = datetime.datetime.now() - timedelta(days=3)
+dtd = datetime.datetime.now() - timedelta(days=4)
+dte = datetime.datetime.now() - timedelta(days=5)
+dtf = datetime.datetime.now() - timedelta(days=6)
+dtg = datetime.datetime.now() - timedelta(days=7)
+
+dtsql_a = dta.strftime('%Y%m%d')
+dtjson_a = dta.strftime('%Y-%m-%d')
+dtsql_b = dtb.strftime('%Y%m%d')
+dtjson_b = dtb.strftime('%Y-%m-%d')
+dtsql_c = dtc.strftime('%Y%m%d')
+dtjson_c = dtc.strftime('%Y-%m-%d')
+dtsql_d = dtd.strftime('%Y%m%d')
+dtjson_d = dtd.strftime('%Y-%m-%d')
+dtsql_e = dte.strftime('%Y%m%d')
+dtjson_e = dte.strftime('%Y-%m-%d')
+dtsql_f = dtf.strftime('%Y%m%d')
+dtjson_f = dtf.strftime('%Y-%m-%d')
+dtsql_g = dtb.strftime('%Y%m%d')
+dtjson_g = dtb.strftime('%Y-%m-%d')
 
 for row in rs:
     s = row[0]
@@ -54,14 +75,35 @@ for row in rs:
         "apikey": api_key }
         response = requests.get(url, data)
         data = response.json()
-        a = (data['Time Series (Daily)'][dtjson])
-        print(a[key]['4. close'] + " " + a[key]['2. high'] + " " + a[key]['5. volume'])
-        pc = a[key]['4. close']
+        a = (data['Time Series (Daily)'][dtjson_a])
+        b = (data['Time Series (Daily)'][dtjson_b])
+        c = (data['Time Series (Daily)'][dtjson_c])
+        d = (data['Time Series (Daily)'][dtjson_d])
+        e = (data['Time Series (Daily)'][dtjson_e])
+        f = (data['Time Series (Daily)'][dtjson_f])
+        g = (data['Time Series (Daily)'][dtjson_g])
+
+        print(a['4. close'] + " " + a['2. high'] + " " + a['5. volume'])
+        pc_a = a['4. close']
+        pc_b = b['4. close']
+        pc_c = c['4. close']
+        pc_d = d['4. close']
+        pc_e = e['4. close']
+        pc_f = f['4. close']
+        pc_g = g['4. close']
+
     except Exception as e:
         print(e)
         pass
 
-    sql_i = "INSERT IGNORE INTO price_instruments_data(symbol, date, price_close) VALUES ('"+s+"','"+ dtsql +"','"+str(pc)+"')"
+    sql_i = "INSERT IGNORE INTO price_instruments_data(symbol, date, price_close) VALUES "+\
+    "('"+s+"','"+ dtsql_a +"','"+str(pc_a)+"'), " +\
+    "('"+s+"','"+ dtsql_b +"','"+str(pc_b)+"'), " +\
+    "('"+s+"','"+ dtsql_c +"','"+str(pc_c)+"'), " +\
+    "('"+s+"','"+ dtsql_d +"','"+str(pc_d)+"'), " +\
+    "('"+s+"','"+ dtsql_e +"','"+str(pc_e)+"'), " +\
+    "('"+s+"','"+ dtsql_f +"','"+str(pc_f)+"'), " +\
+    "('"+s+"','"+ dtsql_g +"','"+str(pc_g)+"')"
     print(sql_i)
     try:
         #cr_i.execute(sql_i)
