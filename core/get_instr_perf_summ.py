4d00d24c1bb3d4f7199c7e2a40ba7253c2aa08eb
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-07 09:38:32 +0700

    dev

--- core/get_instr_perf_summ.py
+++ core/get_instr_perf_summ.py
@@ -34,7 +34,6 @@ def get_pct_from_date(d, sql_select, lp):
                                  cursorclass=pymysql.cursors.DictCursor)
     cr = connection.cursor(pymysql.cursors.SSCursor)
     sql = sql_select + "AND date <= '"+ str(d) +"' ORDER BY date DESC LIMIT 1"
-    print(sql)
     cr.execute(sql)
     rs = cr.fetchall()
     for row in rs:
@@ -44,6 +43,7 @@ def get_pct_from_date(d, sql_select, lp):
     print('pp: ' + str(pp))
     if pp != 0:
         pct = ( (lp - pp) / pp)
+    print(str(pct))
     return pct
 
 def get_prev_session_date(symbol):
