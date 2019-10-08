1bb5cfcd3831c1ea1bad0494888449f403e55c32
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-08 09:04:51 +0700

    dev

--- core/ta_gen_chart_data.py
+++ core/ta_gen_chart_data.py
@@ -57,6 +57,7 @@ def gen_chart(s,uid):
                                      cursorclass=pymysql.cursors.DictCursor)
         cr = connection.cursor(pymysql.cursors.SSCursor)
         sql = "SELECT decimal_places FROM instruments WHERE symbol='"+s+"'"
+        print(sql)
         cr.execute(sql)
         rs = cr.fetchall()
         for row in rs:
@@ -113,6 +114,7 @@ def gen_chart(s,uid):
                                          cursorclass=pymysql.cursors.DictCursor)
             cr_t = connection.cursor(pymysql.cursors.SSCursor)
             sql_t = "DELETE FROM chart_data WHERE uid=" + str(uid)
+            print(sql_t)
             cr_t.execute(sql_t)
             connection.commit()
             cr_t.close()
@@ -263,6 +265,7 @@ def gen_chart(s,uid):
                                                          cursorclass=pymysql.cursors.DictCursor)
                             cr_fp = connection.cursor(pymysql.cursors.SSCursor)
                             sql_fp = "SELECT target_price FROM price_instruments_data WHERE symbol = '"+ str(s) +"' ORDER BY date DESC LIMIT 1"
+                            print(sql_fp)
                             cr_fp.execute(sql_fp)
                             rs_fp = cr_fp.fetchall()
                             for row in rs_fp: forecast = str(row[0])
