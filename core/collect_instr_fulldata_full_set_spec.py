a7dfc104f18b2c2a965c5ad08a63baea90e98543
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-25 15:11:34 +0700

    dev

--- core/collect_instr_fulldata_full_set_spec.py
+++ core/collect_instr_fulldata_full_set_spec.py
@@ -2,14 +2,39 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import sys
+import os
+sys.path.append(os.path.abspath( sett.get_path_pwd() ))
+from sa_access import *
+access_obj = sa_db_access()
 
 from ta_main_update_data import *
+
+sys.path.append(os.path.abspath( sett.get_path_core() ))
+
+db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
+import pymysql.cursors
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 def recalc_histdata(symbol):
     try:
         get_update_instr_data(1,True,symbol)
-    except Exception as e: print(e)
 
+        cr = connection.cursor(pymysql.cursors.SSCursor)
+        sql = 'UPDATE price_instruments_data SET is_ta_calc=0 WHERE symbol = "'+ str(symbol) +'"'
+        cr.execute(sql)
+        connection.commit()
+        cr.close()
+        connection.close()
+
+        print(str(symbol) + ': Done.')
+
+    except Exception as e: print(e)
 
 print("###############################################################################")
 print("Recalculate historical data for specific function")
