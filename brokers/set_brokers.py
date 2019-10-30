33270a471e8a47e956b381f4382c41c60eeed02d
The Cat Tamer <dh@taatu.co>
2019-10-30 10:48:39 +0700

    dev

--- brokers/set_brokers.py
+++ brokers/set_brokers.py
@@ -33,7 +33,9 @@ sql = "DELETE FROM brokers"
 cr.execute(sql)
 
 sql = "INSERT IGNORE INTO brokers(broker_id, burl, affiliate_link) VALUES "+\
-"('eToro','https://www.etoro.com/markets/','http://partners.etoro.com/A52784_TClick.aspx')"
+"('eToro','https://www.etoro.com/markets/','http://partners.etoro.com/A52784_TClick.aspx'),"
+"('googleSiteSmartAlpha','https://sites.google.com/view/','https://sites.google.com/view/about-smartalpha')" +\
+
 debug(sql +": "+ os.path.basename(__file__) )
 
 try:
