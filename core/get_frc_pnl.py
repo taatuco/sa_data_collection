d3d03a0f2cf4dbeea9b8d72687f9e05c39629fff
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-10-31 14:48:33 +0700

    dev

--- core/get_frc_pnl.py
+++ core/get_frc_pnl.py
@@ -20,17 +20,16 @@ from sa_access import *
 access_obj = sa_db_access()
 
 db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
+import pymysql.cursors
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
 def get_forecast_pnl(s,uid,nd, full_update):
-
-    import pymysql.cursors
-    connection = pymysql.connect(host=db_srv,
-                                 user=db_usr,
-                                 password=db_pwd,
-                                 db=db_name,
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
 
     td = datetime.datetime.now()
     i = 0
