81a04b2e2fc4d07da09b8a78ee349d129edd0fe1
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-11-08 11:44:12 +0700

    dev

--- feed/add_feed_type.py
+++ feed/add_feed_type.py
@@ -1,38 +1,43 @@
+""" Import feed type into the database """
 # Copyright (c) 2018-present, Taatu Ltd.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
 import sys
 import os
-
-pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath(pdir) )
-from settings import *
-sett = SmartAlphaPath()
-
-sys.path.append(os.path.abspath( sett.get_path_pwd() ))
-from sa_access import *
-access_obj = sa_db_access()
-
-db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
-
-from pathlib import Path
 import pymysql.cursors
+PDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(PDIR))
+from settings import debug, SmartAlphaPath
+SETT = SmartAlphaPath()
+sys.path.append(os.path.abspath(SETT.get_path_pwd()))
+from sa_access import sa_db_access
+ACCESS_OBJ = sa_db_access()
+DB_USR = ACCESS_OBJ.username()
+DB_PWD = ACCESS_OBJ.password()
+DB_NAME = ACCESS_OBJ.db_name()
+DB_SRV = ACCESS_OBJ.db_server()
 
-def add_feed_type(id,ft):
-    try:
-        connection = pymysql.connect(host=db_srv,
-                                     user=db_usr,
-                                     password=db_pwd,
-                                     db=db_name,
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
-        cr = connection.cursor(pymysql.cursors.SSCursor)
-        sql = "INSERT IGNORE INTO feed_type(id, feed_type) VALUES ('"+str(id)+"','"+ft+"')"
-        cr.execute(sql)
-        connection.commit()
-        debug(sql +": "+ os.path.basename(__file__) )
-        cr.close()
-        connection.close()
-    except Exception as e: debug(e)
+
+def add_feed_type(feed_id, feed_type):
+    """
+    Import brokers and affiliate link to the database.
+    Args:
+        Integer: id for the feed.
+        String: short name or description for the type of feed.
+    Returns:
+        None
+    """
+    connection = pymysql.connect(host=DB_SRV,
+                                 user=DB_USR,
+                                 password=DB_PWD,
+                                 db=DB_NAME,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor(pymysql.cursors.SSCursor)
+    sql = "INSERT IGNORE INTO feed_type(id, feed_type) VALUES ('"+str(feed_id)+"','"+feed_type+"')"
+    cursor.execute(sql)
+    connection.commit()
+    debug(sql +": "+ os.path.basename(__file__))
+    cursor.close()
+    connection.close()
--- labels/set_email_templates.py
+++ labels/set_email_templates.py
@@ -1,55 +1,60 @@
+""" Import email templates into the database """
 # Copyright (c) 2018-present, Taatu Ltd.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
 import sys
 import os
-
-pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.abspath(pdir) )
-from settings import *
-sett = SmartAlphaPath()
-
-sys.path.append(os.path.abspath( sett.get_path_pwd() ))
-from sa_access import *
-access_obj = sa_db_access()
-
-db_usr = access_obj.username(); db_pwd = access_obj.password(); db_name = access_obj.db_name(); db_srv = access_obj.db_server()
-
-from pathlib import Path
-
 import pymysql.cursors
-
-connection = pymysql.connect(host=db_srv,
-                             user=db_usr,
-                             password=db_pwd,
-                             db=db_name,
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-
-cr = connection.cursor(pymysql.cursors.SSCursor)
-sql = "DELETE FROM email_templates"
-cr.execute(sql)
-connection.commit()
+PDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(PDIR))
+from settings import debug, SmartAlphaPath
+SETT = SmartAlphaPath()
+sys.path.append(os.path.abspath(SETT.get_path_pwd()))
+from sa_access import sa_db_access
+ACCESS_OBJ = sa_db_access()
+DB_USR = ACCESS_OBJ.username()
+DB_PWD = ACCESS_OBJ.password()
+DB_NAME = ACCESS_OBJ.db_name()
+DB_SRV = ACCESS_OBJ.db_server()
 
 
-######## English ########
-lang_en = "en"
-new_user_welcome_subject_en = "{fullname}, welcome to SmartAlpha"
-new_user_welcome_content_en = "{fullname},\n Thank you for joining SmartAlpha.\n "+\
-"Your login is your email {email}. We are happy to welcome you as a member of our growing community.\n "+\
-" Feel free to get in touch with us. We are here to support you and to listen."
+def set_email_templates():
+    """
+    Import email text template into the database.
+    Args:
+        None
+    Returns:
+        None
+    """
+    connection = pymysql.connect(host=DB_SRV,
+                                 user=DB_USR,
+                                 password=DB_PWD,
+                                 db=DB_NAME,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-sql = "INSERT IGNORE INTO email_templates(lang, new_user_welcome_subject, new_user_welcome_content) VALUES "+\
-"('"+lang_en+"', '"+  new_user_welcome_subject_en +"', '"+ new_user_welcome_content_en +"' " + ")"
-debug(sql)
-
-try:
-    cr.execute(sql)
+    cursor = connection.cursor(pymysql.cursors.SSCursor)
+    sql = "DELETE FROM email_templates"
+    cursor.execute(sql)
     connection.commit()
-except Exception as e: debug(e)
 
-cr.close()
-connection.close()
+    ######## English ########
+    lang_en = "en"
+    new_user_welcome_subject_en = "{fullname}, welcome to SmartAlpha"
+    new_user_welcome_content_en = "{fullname},\n Thank you for joining SmartAlpha.\n "+\
+    "Your login is your email {email}. We are happy to welcome "+\
+    "you as a member of our growing community.\n "+\
+    " Feel free to get in touch with us. We are here to support you and to listen."
+
+    sql = "INSERT IGNORE INTO email_templates"+\
+    "(lang, new_user_welcome_subject, new_user_welcome_content) VALUES "+\
+    "('"+lang_en+"', '"+  new_user_welcome_subject_en +"', '"+\
+    new_user_welcome_content_en +"' " + ")"
+    debug(sql)
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+set_email_templates()
