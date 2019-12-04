8a7cc681448bc637818c197e26fa9360fbb25ff1
Daiviet Huynh <daiviet.huynh@gmail.com>
2019-12-04 14:19:43 +0700

    dev

--- core/send_email_report.py
+++ core/send_email_report.py
@@ -3,6 +3,14 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import datetime
 from send_intel_report_email import send_intel_report
 
-send_intel_report()
+TODAY_DATE = datetime.datetime.now()
+TODAY_DAY_OF_WEEK = TODAY_DATE.weekday()
+
+SATURDAY_ID = 5
+SUNDAY_ID = 6
+
+if TODAY_DAY_OF_WEEK != 5 and TODAY_DAY_OF_WEEK != 6:
+    send_intel_report()
